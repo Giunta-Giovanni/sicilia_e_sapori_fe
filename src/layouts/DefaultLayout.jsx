1acf39258c3e25a9ba355408cce7f71e1a9feb21
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-06 20:45:32 +0200

    [feat] add condition in default layout to change style on main with different url page

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,20 +1,27 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../components/visual/header/Header";
 import Footer from "../components/visual/footer/Footer";
 import ScrollToTopButton from "../components/visual/scrollToTopButton/ScrollToTopBtn";
 
-
 export default function DefaultLayout() {
+
+    const location = useLocation()
+
+    console.log(location.pathname)
+    // RENDER
     return (
         <>
             <Header />
-
-            <main>
+            <main className={
+                location.pathname == "/it/about-us" || location.pathname == "/en/about-us"
+                    ? 'inverted'
+                    : ''
+            }>
                 <Outlet />
-            </main>
+            </main >
             <ScrollToTopButton />
 
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }
