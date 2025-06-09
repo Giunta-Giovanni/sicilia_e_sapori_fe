8059b0aedcfa23590db9271ede53cd9100987c45
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-09 19:13:35 +0200

    feat: add categories data for menu items

---
+++ src/data/categories.js
@@ -0,0 +1,18 @@
+const categories = [
+    { id: "1", name: "Impasti" },
+    { id: "2", name: "Pizze Gourmet" },
+    { id: "3", name: "Pizza Baciata" },
+    { id: "4", name: "Pizze da Napoli" },
+    { id: "5", name: "Pizze" },
+    { id: "7", name: "Calzoni" },
+    { id: "8", name: "Scaccioni" },
+    { id: "9", name: "Panini" },
+    { id: "10", name: "Piatti" },
+    { id: "11", name: "Friggitoria" },
+    { id: "12", name: "Dessert" },
+    { id: "13", name: "Soft drinks" },
+    { id: "14", name: "Birre" },
+    { id: "15", name: "Vini" }
+];
+
+export { categories };
