e8c09da96b5c50e045a2915f3d4205de69d4e223
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-10 21:14:24 +0200

    refactor allergens pop up for better rendering

--- src/components/visual/allergensPopUp/AllergensPopUp.jsx
+++ src/components/visual/allergensPopUp/AllergensPopUp.jsx
@@ -5,94 +5,53 @@ import styles from './AllergensPopUp.module.css'
 
 export default function AllergenPopUp({ allergens, isAllergenOpen, setIsAllergenOpen, handleClick }) {
 
+    // save lang
     const lang = useLang()
-    console.log(lang);
-
-    // let lang = 'it';
-
 
     // RENDER
     return (
         <>
             {/* popUpWrapper */}
             <div className={styles.popUpWrapper}>
-                {/* Allergens popUp  */}
+                {/* popUp */}
                 <div className={styles.popUp}>
+                    {/* header */}
+                    <div className={styles.header}>
+                        {/* title */}
+                        <h5 className={styles.title}>
+                            {lang === 'it' ? 'Info allergeni' : 'Allergen Information'}
+                        </h5>
 
-                    {lang === 'it' ?
-                        // it
-                        <>
-                            {/* Allergens popUp  */}
-                            < div className={styles.popUp}>
-                                <div className={styles.header}>
-                                    {/* title */}
-                                    <h5 className={styles.title}>Info allergeni</h5>
-                                    {/* closeButton */}
-                                    <button
-                                        className={styles.closeButton}
-                                        onClick={() => handleClick(isAllergenOpen, setIsAllergenOpen)}
-                                    >
-                                    </button>
-                                </div>
+                        {/* closeButton */}
+                        <button
+                            className={styles.closeButton}
+                            onClick={() => handleClick(isAllergenOpen, setIsAllergenOpen)}
+                        >
+                        </button>
+                    </div>
 
+                    <h6>{lang === 'it' ? 'LEGENDA' : 'LEGEND'}</h6>
+                    {/* legend */}
+                    <div className={styles.legend}>
+                        <ul>
+                            {allergens.map(allergen => (
+                                <li key={allergen.key}>
+                                    <img src={allergen.icon} alt={allergen.key} />
+                                    <span>{allergen.label[lang]}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-                                {/* legend */}
-                                <h6>LEGENDA</h6>
-                                <div className={styles.legend}>
-                                    <ul>
-                                        {allergens.map(allergen => {
-                                            return (
-                                                <>
-                                                    <li key={allergen.key}>
-                                                        <img src={allergen.icon} alt="" />
-                                                        <span>
-                                                            {allergen.label.it}
-                                                        </span>
-                                                    </li>
-
-                                                </>
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-
-                                {/* regulations */}
-                                <p className={styles.regulation}>
-                                    In ottemperanza al Regolamento UE 1169/2011, informiamo la gentile clientela che alcuni piatti possono contenere allergeni. Di seguito è riportata la legenda degli allergeni rappresentati con icone.
-                                </p>
-                            </div>
-                        </>
-                        :
-                        // en
-                        <>
-
-                            {/* title */}
-                            <h4 className={styles.title}>Allergen Information</h4>
-                            {/* regulation */}
-                            <p className={styles.regulation}>
-                                In compliance with EU Regulation 1169/2011, we inform our valued customers that some dishes may contain allergens. Below is the legend of allergens represented by icons.
-                            </p>
-                            {/* legend */}
-                            <div className={styles.legend}>
-                                <h5>LEGEND</h5>
-                                <ul>
-                                    {allergens.map(allergen => {
-                                        return (
-                                            <li key={allergen.key}>
-                                                {allergen.label.en}
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                        </>
-                    }
-
-
-
-                </div >
+                    {/* regulation */}
+                    <p className={styles.regulation}>
+                        {lang === 'it'
+                            ? "In ottemperanza al Regolamento UE 1169/2011, informiamo la gentile clientela che alcuni piatti possono contenere allergeni. Al di sopra è riportata la legenda degli allergeni rappresentati con icone."
+                            : "In compliance with EU Regulation 1169/2011, we inform our valued customers that some dishes may contain allergens. The icons above represent the allergen legend."
+                        }
+                    </p>
+                </div>
             </div>
-
         </>
-    )
+    );
 }
