9809661cc897eb43d43454329828da6a9a37821e
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-12 13:06:08 +0200

    feat: update categories data structure to include Italian and English names

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,18 +1,18 @@
 const categories = [
-    { id: "1", name: "Impasti" },
-    { id: "2", name: "Pizze Gourmet" },
-    { id: "3", name: "Pizza Baciata" },
-    { id: "4", name: "Pizze da Napoli" },
-    { id: "5", name: "Pizze" },
-    { id: "7", name: "Calzoni" },
-    { id: "8", name: "Scaccioni" },
-    { id: "9", name: "Panini" },
-    { id: "10", name: "Piatti" },
-    { id: "11", name: "Friggitoria" },
-    { id: "12", name: "Dessert" },
-    { id: "13", name: "Soft drinks" },
-    { id: "14", name: "Birre" },
-    { id: "15", name: "Vini" }
+    { id: "1", name_it: "Impasti", name_en: "Doughs" },
+    { id: "2", name_it: "Pizze Gourmet", name_en: "Gourmet Pizzas" },
+    { id: "3", name_it: "Pizza Baciata", name_en: "Stuffed Pizza" },
+    { id: "4", name_it: "Pizze da Napoli", name_en: "Pizzas from Naples" },
+    { id: "5", name_it: "Pizze", name_en: "Pizzas" },
+    { id: "7", name_it: "Calzoni", name_en: "Calzones" },
+    { id: "8", name_it: "Scaccioni", name_en: "Scaccioni" },
+    { id: "9", name_it: "Panini", name_en: "Sandwiches" },
+    { id: "10", name_it: "Piatti", name_en: "Dishes" },
+    { id: "11", name_it: "Friggitoria", name_en: "Fried Food" },
+    { id: "12", name_it: "Dessert", name_en: "Desserts" },
+    { id: "13", name_it: "Soft drinks", name_en: "Soft Drinks" },
+    { id: "14", name_it: "Birre", name_en: "Beers" },
+    { id: "15", name_it: "Vini", name_en: "Wines" }
 ];
 
 export { categories };
