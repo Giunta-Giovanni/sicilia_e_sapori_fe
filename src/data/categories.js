fe16140a33ab28766d23538713d4f19e5f0fb38b
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-16 20:00:06 +0200

    [feat] add categories data for menu rendering and refactor MenuPage to use external categories

---
+++ src/data/categories.js
@@ -0,0 +1,23 @@
+// create categorie for next rendering
+const categories = [
+    { id: 1, key: 'gourmet', title_it: 'Pizze Gourmet', title_en: 'Gourmet Pizzas' },
+    { id: 2, key: 'stuffed', title_it: 'Pizze Baciate', title_en: 'Stuffed Pizzas' },
+    { id: 3, key: 'naples', title_it: 'Pizze da Napoli', title_en: 'Pizzas from Naples' },
+    { id: 4, key: 'pizzas', title_it: 'Pizze', title_en: 'Pizzas', subtitle_it: 'Pizze Rosse', subtitle_en: 'Red Pizzas' },
+    { id: 5, key: 'whitePizzas', title_it: null, title_en: null, subtitle_it: 'Pizze Bianche', subtitle_en: 'White Pizzas' },
+    { id: 6, key: 'seaPizzas', title_it: null, title_en: null, subtitle_it: 'Pizze al Pesce', subtitle_en: 'Sea Food Pizzas' },
+    { id: 19, key: 'buffaloPizzas', title_it: null, title_en: null, subtitle_it: 'Pizze con Mozzarella di Bufala', subtitle_en: 'Pizzas with Buffalo Cheese' },
+    { id: 7, key: 'calzones', title_it: 'Calzoni', title_en: 'Calzones', subtitle_it: null, subtitle_en: null },
+    { id: 8, key: 'scaccioni', title_it: 'Scaccioni', title_en: 'Scaccioni' },
+    { id: 9, key: 'sandwiches', title_it: 'Panini', title_en: 'Sandwiches' },
+    { id: 10, key: 'dishes', title_it: 'Piatti', title_en: 'Dishes' },
+    { id: 11, key: 'friedFood', title_it: 'Friggitoria', title_en: 'Fried Food' },
+    { id: 12, key: 'desserts', title_it: 'Dolci', title_en: 'Desserts' },
+    { id: 14, key: 'beers', title_it: 'Birre', title_en: 'Beers' },
+    { id: 15, key: 'wines', title_it: 'Vini', title_en: 'Wines', subtitle_it: 'Vini Rossi', subtitle_en: 'Red Wines' },
+    { id: 16, key: 'WhiteWines', title_it: null, title_en: null, subtitle_it: 'Vini Bianchi', subtitle_en: 'White Wines' },
+    { id: 17, key: 'SparklingWines', title_it: null, title_en: null, subtitle_it: 'Vini Frizzanti', subtitle_en: 'Sparkling Wines' },
+    { id: 18, key: 'softDrinks', title_it: 'Soft Drinks', title_en: 'Soft Drinks' },
+];
+
+export { categories };
--- src/pages/menuPage/MenuPage.jsx
+++ src/pages/menuPage/MenuPage.jsx
@@ -1,19 +1,21 @@
 // import Hooks
 import { useRef, useState, useEffect, useMemo } from "react";
-import MenuContext from "../../context/MenuContext";
 import useLang from '../../hooks/useLang';
 
-// import axios
+// import Context
+import MenuContext from "../../context/MenuContext";
+
+// import Axios
 import axios from 'axios';
 
 // import Sections
 import MenuSection from "../../sections/MenuSection"
 
-// import components
+// import Components
 import SlowScrollTo from "../../components/technical/SlowScrollTo";
 
-// import Styles
-import styles from "./MenuPage.module.css"
+// import Data
+import { categories } from '../../data/categories';
 
 // import Assets
 import { allergens } from '../../assets/svg/allergens/allergens';
@@ -23,6 +25,11 @@ import allergensDoc from '../../assets/svg/general/doc.svg';
 import arrowDown from '../../assets/svg/general/arrow-down.svg';
 import filterIcon from '../../assets/svg/general/filter.svg';
 import plate from '../../assets/svg/general/plate.svg';
+
+// import Styles
+import styles from "./MenuPage.module.css";
+
+
 
 export default function MenuPage() {
 
@@ -58,28 +65,6 @@ export default function MenuPage() {
         softDrinks: useRef(null),
     };
 
-
-    const categories = [
-        { id: 1, key: 'gourmet', title_it: 'Pizze Gourmet', title_en: 'Gourmet Pizzas' },
-        { id: 2, key: 'stuffed', title_it: 'Pizze Baciate', title_en: 'Stuffed Pizzas' },
-        { id: 3, key: 'naples', title_it: 'Pizze da Napoli', title_en: 'Pizzas from Naples' },
-        { id: 4, key: 'pizzas', title_it: 'Pizze', title_en: 'Pizzas', subtitle_it: 'Pizze Rosse', subtitle_en: 'Red Pizzas' },
-        { id: 5, key: 'whitePizzas', title_it: null, title_en: null, subtitle_it: 'Pizze Bianche', subtitle_en: 'White Pizzas' },
-        { id: 6, key: 'seaPizzas', title_it: null, title_en: null, subtitle_it: 'Pizze al Pesce', subtitle_en: 'Sea Food Pizzas' },
-        { id: 19, key: 'buffaloPizzas', title_it: null, title_en: null, subtitle_it: 'Pizze con Mozzarella di Bufala', subtitle_en: 'Pizzas with Buffalo Cheese' },
-        { id: 7, key: 'calzones', title_it: 'Calzoni', title_en: 'Calzones', subtitle_it: null, subtitle_en: null },
-        { id: 8, key: 'scaccioni', title_it: 'Scaccioni', title_en: 'Scaccioni' },
-        { id: 9, key: 'sandwiches', title_it: 'Panini', title_en: 'Sandwiches' },
-        { id: 10, key: 'dishes', title_it: 'Piatti', title_en: 'Dishes' },
-        { id: 11, key: 'friedFood', title_it: 'Friggitoria', title_en: 'Fried Food' },
-        { id: 12, key: 'desserts', title_it: 'Dolci', title_en: 'Desserts' },
-        { id: 14, key: 'beers', title_it: 'Birre', title_en: 'Beers' },
-        { id: 15, key: 'wines', title_it: 'Vini', title_en: 'Wines', subtitle_it: 'Vini Rossi', subtitle_en: 'Red Wines' },
-        { id: 16, key: 'WhiteWines', title_it: null, title_en: null, subtitle_it: 'Vini Bianchi', subtitle_en: 'White Wines' },
-        { id: 17, key: 'SparklingWines', title_it: null, title_en: null, subtitle_it: 'Vini Frizzanti', subtitle_en: 'Sparkling Wines' },
-        { id: 18, key: 'softDrinks', title_it: 'Soft Drinks', title_en: 'Soft Drinks' },
-    ];
-
     // save product in specific categories
     const categorizedProducts = useMemo(() => {
         if (!Array.isArray(products)) return {}; // fallback protettivo
@@ -101,8 +86,6 @@ export default function MenuPage() {
         ref: category.key ? sections[category.key] : undefined
     }));
     console.log('questi sono i productCategories completi', productCategories)
-
-
 
     useEffect(() => {
         fetchProducts()
