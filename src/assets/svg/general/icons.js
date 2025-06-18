3dfcb2b21ea1bbd148e3a0853eeffc3cfe59e05f
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-18 18:17:45 +0200

    change name to  SVG icons and create icons.js module
    - Created a new icons.js file to import and export various SVG icons used in the application.

---
+++ src/assets/svg/general/icons.js
@@ -0,0 +1,43 @@
+// import svg
+import allergensDoc from './doc.svg';
+import arrowDown from './arrow-down.svg';
+import delivery from './delivery.svg';
+import facebookBrown from './facebook-brown.svg';
+import facebookWhite from './facebook-white.svg';
+import filterIcon from './filter.svg';
+import instagramBrown from './instagram-brown.svg';
+import instagramWhite from './instagram-white.svg';
+import mapsBrown from './maps-brown.svg';
+import mapsWhite from './maps-white.svg';
+import numb1 from './1.svg';
+import numb2 from './2.svg';
+import onPlace from './on-place.svg';
+import pizzaPeel from './pizza-peel.svg';
+import plate from './plate.svg';
+import spicy from './pepper.svg';
+import tripAdvisorBrown from './trip-advisor-brown.svg';
+import tripAdvisorWhite from './trip-advisor-white.svg';
+import vegetarian from './leaf.svg';
+
+// export object
+export const icons = {
+    allergensDoc,
+    arrowDown,
+    delivery,
+    facebookBrown,
+    facebookWhite,
+    filterIcon,
+    instagramBrown,
+    instagramWhite,
+    mapsBrown,
+    mapsWhite,
+    numb1,
+    numb2,
+    onPlace,
+    pizzaPeel,
+    plate,
+    spicy,
+    tripAdvisorBrown,
+    tripAdvisorWhite,
+    vegetarian
+};
