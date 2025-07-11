cfb418c906203359c131404d51d8343b2f64a345
GiuntaGiovanni <jhonniest741@gmail.com>
2025-07-11 16:31:53 +0200

    feat: add new video and image assets for location and tournament sections

---
+++ src/assets/location/location.js
@@ -0,0 +1,20 @@
+// import jpg
+import bancone from './bancone.jpg';
+import dettagli from './dettagli.jpg';
+import esterno from './esterno.JPG';
+import forno from './forno.JPG';
+import ingresso from './ingresso.JPG';
+import veranda from './veranda.JPG';
+
+
+
+
+// export object
+export const location = {
+    bancone,
+    dettagli,
+    esterno,
+    forno,
+    ingresso,
+    veranda,
+};
---
+++ src/assets/torneo/tournaments.js
@@ -0,0 +1,22 @@
+// import jpg
+import img1 from './img1.jpg';
+import img2 from './img2.jpg';
+import img3 from './img3.JPG';
+import img4 from './img4.JPG';
+import img5 from './img5.JPG';
+import img6 from './img6.JPG';
+import img7 from './img7.JPG';
+import img8 from './img8.JPG';
+
+
+
+// export object
+export const tournaments = {
+    img1,
+    img2,
+    img3,
+    img5,
+    img6,
+    img7,
+    img8
+};
