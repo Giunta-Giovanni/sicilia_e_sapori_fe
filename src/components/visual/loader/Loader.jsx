fd7aeae0d75e06a05c781561e9b3622cb2e2b24a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-08-23 19:10:10 +0200

    [feat] add dark theme

--- src/components/visual/loader/Loader.jsx
+++ src/components/visual/loader/Loader.jsx
@@ -1,17 +1,25 @@
+// import hooks
+import { useContext } from 'react';
+
+// import context
+import GlobalContext from '../../../context/GlobalContext';
 // import Styles
 import styles from './Loader.module.css';
 
 // import Assets
-import logo from '../../../assets/svg/logo/logo-primary-brown.svg';
+import logoBrown from '../../../assets/svg/logo/logo-primary-brown.svg';
+import logoWhite from '../../../assets/svg/logo/logo-primary-white.svg';
+
 
 export default function Loader({ isReady }) {
 
+    const {isDarkMode} = useContext(GlobalContext)
     // RENDERING
     return (
 
         // loader
         <div className={`${styles.loader} ${isReady ? styles.fadeOut : ''}`}>
-            <img src={logo} alt="Loading..." />
+            <img src={isDarkMode?logoWhite:logoBrown} alt="Loading..." />
         </div>
     )
 }
