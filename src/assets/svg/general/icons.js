8d014293b62d99f2a69754358943f96759543620
GiuntaGiovanni <jhonniest741@gmail.com>
2025-08-23 19:02:38 +0200

    [feat] add white image for conditin rendering

--- src/assets/svg/general/icons.js
+++ src/assets/svg/general/icons.js
@@ -1,13 +1,17 @@
 // import svg
-import allergensDoc from './doc.svg';
+import allergensDocBrown from './doc-brown.svg';
+import allergensDocWhite from './doc-white.svg';
+
 import arrowDownBrown from './arrow-down-brown.svg';
 import arrowDownWhite from './arrow-down-white.svg';
 import deliveryBrown from './delivery-brown.svg';
 import deliveryWhite from './delivery-white.svg';
 import facebookBrown from './facebook-brown.svg';
 import facebookWhite from './facebook-white.svg';
-import filterIcon from './filter.svg';
-import glutenFree from './gluten-free.svg';
+import filterIconBrown from './filter-brown.svg';
+import filterIconWhite from './filter-white.svg';
+import glutenFreeBrown from './gluten-free-brown.svg';
+import glutenFreeWhite from './gluten-free-white.svg';
 import instagramBrown from './instagram-brown.svg';
 import instagramWhite from './instagram-white.svg';
 import mapsBrown from './maps-brown.svg';
@@ -16,33 +20,41 @@ import numb1 from './1.svg';
 import numb2 from './2.svg';
 import onPlaceBrown from './on-place-brown.svg';
 import onPlaceWhite from './on-place-white.svg';
-import oven from './oven.svg';
+import ovenWhite from './oven-white.svg';
+import ovenBrown from './oven-white.svg';
 import phoneBrown from './phone-brown.svg';
 import phoneWhite from './phone-white.svg'
 import pizzaBoxBrown from './pizza-box-brown.svg';
 import pizzaBoxWhite from './pizza-box-white.svg';
-
 import pizzaPeel from './pizza-peel.svg';
-import plate from './plate.svg';
-import rustic from './rustic.svg';
+import plateBrown from './plate-brown.svg';
+import plateWhite from './plate-white.svg';
+import rusticBrown from './rustic-brown.svg';
+import rusticWhite from './rustic-white.svg';
 import spicy from './pepper.svg';
-import takeAway from './take-away.svg'
+import takeAwayBrown from './take-away-brown.svg'
+import takeAwayWhite from './take-away-white.svg'
 import tripAdvisorBrown from './trip-advisor-brown.svg';
 import tripAdvisorWhite from './trip-advisor-white.svg';
 import vegetarian from './leaf.svg';
-import wholeWheat from './whole-wheat.svg';
+import wholeWheatBrown from './whole-wheat-brown.svg';
+import wholeWheatWhite from './whole-wheat-white.svg';
+
 
 // export object
 export const icons = {
-    allergensDoc,
+    allergensDocWhite,
+    allergensDocBrown,
     arrowDownBrown,
     arrowDownWhite,
     deliveryBrown,
     deliveryWhite,
     facebookBrown,
     facebookWhite,
-    filterIcon,
-    glutenFree,
+    filterIconBrown,
+    filterIconWhite,
+    glutenFreeBrown,
+    glutenFreeWhite,
     instagramBrown,
     instagramWhite,
     mapsBrown,
@@ -51,19 +63,23 @@ export const icons = {
     numb2,
     onPlaceBrown,
     onPlaceWhite,
-
-    oven,
+    ovenBrown,
+    ovenWhite,
     phoneBrown,
     phoneWhite,
     pizzaBoxBrown,
     pizzaBoxWhite,
     pizzaPeel,
-    plate,
-    rustic,
+    plateBrown,
+    plateWhite,
+    rusticBrown,
+    rusticWhite,
     spicy,
-    takeAway,
+    takeAwayBrown,
+    takeAwayWhite,
     tripAdvisorBrown,
     tripAdvisorWhite,
     vegetarian,
-    wholeWheat,
+    wholeWheatBrown,
+    wholeWheatWhite,
 };
